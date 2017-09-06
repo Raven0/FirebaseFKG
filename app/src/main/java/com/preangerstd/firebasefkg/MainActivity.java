78d835e60f3b2226e22e170410f3ac8334651454
Ismail Rohaga <azh.zhafir@gmail.com>
2017-09-06 18:36:15 +0700

    commit, ATTENTION
    make sure firebase auth, database & storage version is the same as gms play services version

--- app/src/main/java/com/preangerstd/firebasefkg/MainActivity.java
+++ app/src/main/java/com/preangerstd/firebasefkg/MainActivity.java
@@ -2,9 +2,8 @@ package com.preangerstd.firebasefkg;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -81,7 +81,13 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+
+            /*LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.content_main, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);*/
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
