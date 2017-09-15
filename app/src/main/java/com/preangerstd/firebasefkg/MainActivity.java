b1a6c86b86e30a4589f5bfa690030a1d791450fd
Ismail Rohaga <azh.zhafir@gmail.com>
2017-09-15 20:55:43 +0700

    nyobain revert
    aja

--- app/src/main/java/com/preangerstd/firebasefkg/MainActivity.java
+++ app/src/main/java/com/preangerstd/firebasefkg/MainActivity.java
@@ -1,13 +1,73 @@
 package com.preangerstd.firebasefkg;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null){
+                    /*Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);*/
+                }
+            }
+        };
+        checkUserExist();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        /*if(mAuth.getCurrentUser() != null){
+        }*/
+
+        mAuth.addAuthStateListener(mAuthListener);
+
+
+
+    }
+
+    private void checkUserExist() {
+        if(mAuth.getCurrentUser() != null) {
+            final String userid = mAuth.getCurrentUser().getUid();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.action_logout){
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        mAuth.signOut(); //make sure AuthStateListener is added
     }
 }
