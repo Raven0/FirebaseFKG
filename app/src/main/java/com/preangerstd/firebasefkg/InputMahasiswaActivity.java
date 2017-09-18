fef07b031b808085a0418701adb08dde0339fd4d
Ismail Rohaga <azh.zhafir@gmail.com>
2017-09-18 21:23:56 +0700

    ready
    backup dulu

--- app/src/main/java/com/preangerstd/firebasefkg/InputMahasiswaActivity.java
+++ app/src/main/java/com/preangerstd/firebasefkg/InputMahasiswaActivity.java
@@ -1,13 +1,128 @@
 package com.preangerstd.firebasefkg;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 
 public class InputMahasiswaActivity extends AppCompatActivity {
+
+    private Calendar myCalendar = Calendar.getInstance();
+    private EditText edittext;
+    private DatabaseReference mDatabaseProdi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_mahasiswa);
+
+        //TGL LAHIR
+
+        edittext= (EditText) findViewById(R.id.tbTglMahasiswa);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        edittext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(InputMahasiswaActivity.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        // KEWARGANEGARAAN
+
+        Locale[] locale = Locale.getAvailableLocales();
+        ArrayList<String> countries = new ArrayList<String>();
+        String country;
+        for( Locale loc : locale ){
+            country = loc.getDisplayCountry();
+            if( country.length() > 0 && !countries.contains(country) ){
+                countries.add( country );
+            }
+        }
+        Collections.sort(countries, String.CASE_INSENSITIVE_ORDER);
+
+        Spinner citizenship = (Spinner)findViewById(R.id.cbNegaraMahasiswa);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, countries);
+        citizenship.setAdapter(adapter);
+
+        //ANGKATAN
+
+
+        ArrayList<String> years = new ArrayList<String>();
+        int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = 2000; i <= thisYear; i++) {
+            years.add(Integer.toString(i));
+        }
+        ArrayAdapter<String> adapterAngkatan = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, years);
+
+        Spinner spinYear = (Spinner)findViewById(R.id.cbAngkatanMahasiswa);
+        spinYear.setAdapter(adapterAngkatan);
+
+        //Firebase Start Here
+
+        mDatabaseProdi = FirebaseDatabase.getInstance().getReference().child("tbProdi");
+
+        // PROGRAM STUDI
+
+        mDatabaseProdi.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Is better to use a List, because you don't know the size
+                // of the iterator returned by dataSnapshot.getChildren() to
+                // initialize the array
+                final List<String> prodi = new ArrayList<String>();
+
+                for (DataSnapshot areaSnapshot: dataSnapshot.getChildren()) {
+                    String prodiName = areaSnapshot.child("programStudi").getValue(String.class);
+                    prodi.add(prodiName);
+                }
+
+                Spinner areaSpinner = (Spinner) findViewById(R.id.cbProdiMahasiswa);
+                ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(InputMahasiswaActivity.this, android.R.layout.simple_spinner_item, prodi);
+                areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                areaSpinner.setAdapter(areasAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat);
+        edittext.setText(sdf.format(myCalendar.getTime()));
     }
 }
